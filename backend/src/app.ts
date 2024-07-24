dcbfec6d845d1e3cce118a631ab261f5da9d45c2
TPiber <gabrieltpiber@outlook.com>
2024-07-24 06:03:43 -0300

    chore: adding cors and removing body-parser and dotenv

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,7 @@
 import express from "express";
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
-
-dotenv.config();
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 export default app;
