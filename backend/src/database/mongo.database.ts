413402d2a1098aff531c51df96a11b4fe55d6e58
TPiber <gabrieltpiber@outlook.com>
2024-07-24 05:54:56 -0300

    chore: adding Mongo and creating starting methods

---
+++ backend/src/database/mongo.database.ts
@@ -0,0 +1,51 @@
+import { Document, MongoClient, ObjectId } from "mongodb";
+
+export class MongoDatabase {
+  private client: MongoClient;
+
+  constructor() {
+    /* Typically, environment variables are used to store the connection string for security reasons. However, for this test, it will be exposed since the database will be deleted later. */
+    this.client = new MongoClient(
+      "mongodb+srv://admin:admin@cluster0.kfnwdny.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
+  }
+
+  // Method to establish a connection to the MongoDB client
+  async connect() {
+    await this.client.connect();
+  }
+
+  // Method to close the connection to the MongoDB client
+  async disconnect() {
+    await this.client.close();
+  }
+
+  // Retrieves a specific collection from the database by its name
+  getCollection(collectionName: string) {
+    return this.client.db("mydb").collection(collectionName);
+  }
+
+  // Creates a new document in the specified collection
+  async create(collectionName: string, insert: Document) {
+    const result = await this.getCollection(collectionName).insertOne(insert);
+    return result.insertedId; // Returns the ID of the inserted document
+  }
+
+  // Fetches all documents from a specified collection
+  async getDocuments(collectionName: string) {
+    return await this.getCollection(collectionName).find().toArray();
+  }
+
+  // Deletes a document from a specified collection by its ID
+  async delete(collectionName: string, id: ObjectId) {
+    return await this.getCollection(collectionName).deleteOne({ _id: id });
+  }
+
+  // Updates a document in a specified collection by its ID
+  async update(collectionName: string, id: ObjectId, update: Document) {
+    return await this.getCollection(collectionName).updateOne(
+      { _id: id },
+      { $set: update } // Uses the $set operator to update the document
+    );
+  }
+}
